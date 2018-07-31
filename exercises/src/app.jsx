856085e31713d9da3268f9ec470209a136969996
jaehyunghwang <jh62@gmail.com>
2018-07-31 13:06:41 -0500

    profile page - not tested

--- exercises/src/app.jsx
+++ exercises/src/app.jsx
@@ -1,45 +1,51 @@
-import React from 'react';
-import { Grid, Navbar, Nav, NavItem, } from 'react-bootstrap';
-import { BrowserRouter, Route } from 'react-router-dom';
-import { Provider, observer } from 'mobx-react';
-import { action } from 'mobx';
-import UX from './ux';
-import Search from './components/search';
-import Exercise from './components/exercise';
-import Preview from './components/preview';
-import QK from './components/qk';
-import QKControls from './components/qk/controls/controls';
-import QKQuestionRoute from './components/qk/questionroute/QuestionRoute';
-import QKNewQuestion from './components/qk/newquestion/NewQuestion';
-import QKHelp from './components/qk/help/Help';
-import QKSubjectPicker from './components/qk/subjectpicker/SubjectPicker';
-import UserActionsMenu from './components/user-actions-menu';
+import React from "react";
+import { Grid, Navbar, Nav, NavItem } from "react-bootstrap";
+import { BrowserRouter, Route } from "react-router-dom";
+import { Provider, observer } from "mobx-react";
+import { action } from "mobx";
+import UX from "./ux";
+import Search from "./components/search";
+import Exercise from "./components/exercise";
+import Preview from "./components/preview";
+import QK from "./components/qk";
+import QKControls from "./components/qk/controls/controls";
+import QKQuestionRoute from "./components/qk/questionroute/QuestionRoute";
+import QKNewQuestion from "./components/qk/newquestion/NewQuestion";
+import QKHelp from "./components/qk/help/Help";
+import QKProfile from "./components/qk/profile/Profile";
+import QKSubjectPicker from "./components/qk/subjectpicker/SubjectPicker";
+import UserActionsMenu from "./components/user-actions-menu";
 
 export default class App extends React.Component {
-
   ux = new UX();
 
-  @action.bound onNav(ev) {
+  @action.bound
+  onNav(ev) {
     ev.preventDefault();
     this.router.history.push(ev.currentTarget.pathname);
   }
 
   render() {
-    const { ux, props: { data: { user } } } = this;
-    const inQK = window.location.pathname.includes('/qk')
+    const {
+      ux,
+      props: {
+        data: { user }
+      }
+    } = this;
+    const inQK = window.location.pathname.includes("/qk");
 
     return (
       <Provider ux={ux}>
-        <BrowserRouter ref={br => this.router = br}>
+        <BrowserRouter ref={br => (this.router = br)}>
           <Grid fluid className={"exercises" + (inQK ? " qk" : "")}>
-            {!inQK ?
+            {!inQK ? (
               <Navbar fixedTop>
                 <Navbar.Header>
                   <Navbar.Brand>
                     <a href="#home">OX Exercises</a>
                   </Navbar.Brand>
                 </Navbar.Header>
-                <Nav className="exercise-navbar-controls" >
+                <Nav className="exercise-navbar-controls">
                   <NavItem onClick={this.onNav} href="/search">
                     Search
                   </NavItem>
@@ -52,21 +58,23 @@ export default class App extends React.Component {
                 </Nav>
                 <UserActionsMenu user={user} />
               </Navbar>
-              : <Route path="/qk" component={QKControls} /> }
+            ) : (
+              <Route path="/qk" component={QKControls} />
+            )}
             <div className={"exercises-body" + (inQK ? " qk" : "")}>
               <Route path="/search" component={Search} />
               <Route path="/exercise/:uid" component={Exercise} />
               <Route path="/preview/:uid" component={Preview} />
               <Route path="/qk/home" component={QK} />
-              <Route path='/qk/questions' component={QKQuestionRoute}/>
-              <Route path='/qk/new-question' component={QKNewQuestion}/>
-              <Route path='/qk/help' component={QKHelp}/>
-              <Route path='/qk/subject-picker'component={QKSubjectPicker}/>
+              <Route path="/qk/questions" component={QKQuestionRoute} />
+              <Route path="/qk/new-question" component={QKNewQuestion} />
+              <Route path="/qk/help" component={QKHelp} />
+              <Route path="/qk/subject-picker" component={QKSubjectPicker} />
+              <Route path="/qk/profile" component={QKProfile} />
             </div>
           </Grid>
         </BrowserRouter>
       </Provider>
     );
   }
-
 }
---
+++ exercises/src/components/qk/profile/Profile.jsx
@@ -0,0 +1,164 @@
+import React from "react";
+import "./Profile.css";
+import {
+  TabContent,
+  TabPane,
+  Nav,
+  NavItem,
+  NavLink,
+  Card,
+  Button,
+  CardTitle,
+  CardText,
+  Row,
+  Col,
+  CardImg,
+  CardBody,
+  CardSubtitle,
+  Jumbotron,
+  Progress
+} from "reactstrap";
+import classnames from "classnames";
+import { LinkContainer } from "react-router-bootstrap";
+export default class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      activeTab: "1"
+    };
+  }
+  toggle(tab) {
+    if (this.state.activeTab !== tab) {
+      this.setState({
+        activeTab: tab
+      });
+    }
+  }
+  render() {
+    return (
+      <div>
+        <Nav tabs>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === "1" })}
+              onClick={() => {
+                this.toggle("1");
+              }}
+            >
+              Profile
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === "2" })}
+              onClick={() => {
+                this.toggle("2");
+              }}
+            >
+              Questions
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === "3" })}
+              onClick={() => {
+                this.toggle("3");
+              }}
+            >
+              Draft
+            </NavLink>
+          </NavItem>
+        </Nav>
+        <TabContent activeTab={this.state.activeTab}>
+          <TabPane tabId="1">
+            <Row>
+              <Col sm="12">
+                <div>
+                  <Jumbotron id="jumbo">
+                    <h1 className="display-3">
+                      Welcome to Question Kitchen, John!
+                    </h1>
+                    <p className="lead">
+                      This is a page where you can see your personal
+                      information, your progress, create new set of questions,
+                      and go through your stored questions
+                    </p>
+                    <hr className="my-2" />
+                    <p>
+                      <div>
+                        <div className="text-center">Your Progress</div>
+                        <Progress multi>
+                          <Progress bar value="15">
+                            Calculus
+                          </Progress>
+                          <Progress bar color="success" value="30">
+                            English
+                          </Progress>
+                          <Progress bar color="info" value="25">
+                            Biology
+                          </Progress>
+                          <Progress bar color="warning" value="20">
+                            Statistics
+                          </Progress>
+                          <Progress bar color="danger" value="5">
+                            Chemistry
+                          </Progress>
+                        </Progress>
+                      </div>
+                    </p>
+                    <p className="lead">
+                      <Button color="primary">Learn More</Button>
+                    </p>
+                  </Jumbotron>
+                  <div id="face">
+                    <Card>
+                      <CardImg
+                        id="img"
+                        top
+                        width="100%"
+                        src="http://www.piachievers.com/img/users-female-2.png"
+                        alt="Card image cap"
+                      />
+                      <CardBody>
+                        <CardTitle id="jumbo">John King </CardTitle>
+                        <CardSubtitle id="jumbo">
+                          Cascade High School
+                        </CardSubtitle>
+                        <CardSubtitle id="jumbo">English teacher</CardSubtitle>
+                        <CardText />
+                        <Button>Edit</Button>
+                      </CardBody>
+                    </Card>
+                  </div>
+                </div>
+              </Col>
+            </Row>
+          </TabPane>
+          <TabPane tabId="2">
+            <Row>
+              <Col sm="6">
+                <Card body>
+                  <CardTitle>Create New Questions</CardTitle>
+                  <CardText>
+                    Create new set of questions and get more credit!
+                  </CardText>
+                  <LinkContainer to="/qk/new-question">
+                    <Button>Create a question</Button>
+                  </LinkContainer>
+                </Card>
+              </Col>
+              <Col sm="6">
+                <Card body>
+                  <CardTitle>Question Library</CardTitle>
+                  <CardText>Your personal favorite questions</CardText>
+                  <Button>Questions saved</Button>
+                </Card>
+              </Col>
+            </Row>
+          </TabPane>
+        </TabContent>
+      </div>
+    );
+  }
+}
